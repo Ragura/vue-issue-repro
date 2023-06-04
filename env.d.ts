06097b548da0d822dc2151a249ecd9d0a4930171
Ragura <ragura@gmail.com>
2023-06-04 17:21:01 +0200

    remove global component

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="vite/client" />
-import Dropdown from './src/components/DropdownDefineModel.vue'
-
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    Dropdown: typeof Dropdown,
-  }
-}
